7b858ad4cfa9b929ae4234e6c28a983afe782a43
Diego <maciasg.diego17@gmail.com>
2024-04-01 22:35:48 -0500

    Finished 16x16 grid

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// Create rows and columns
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('grid-row');
-    container.appendChild(row);
+document.addEventListener('DOMContentLoaded', function() {
+const container = document.querySelector('.container');
 
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement('div');
-        row.appendChild(cell);
+    // Create rows and columns
+    for (let i = 0; i < 16; i++) {
+        const row = document.createElement('div');
+        row.classList.add('grid-row');
+        container.appendChild(row);
+
+        // Create divs inside each row
+        for (let j = 0; j < 16; j++) { 
+            const cell = document.createElement('div');
+            row.appendChild(cell);
+        }
     }
-}
+});
